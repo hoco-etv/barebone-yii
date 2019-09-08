05aa02419af73212254419d1de32fad73709eb18
Bram <bramdenouden96@hotmail.com>
2019-09-08 20:23:48 +0200

    removed other brands from the barebone template

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,38 +26,28 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 <div class="wrap">
-    <section id='klushok-navbar'>
-        <?php
-        NavBar::begin([
-            'brandLabel' => Html::img('@web/img/klushok-logo.png',['alt'=>Yii::$app->name, 'class' => "logo_flag"]),
-            'brandUrl' => Yii::$app->homeUrl,
-            'options' => [
-                'id' => 'klushok-navbar'
-                //'class' => 'navbar-inverse navbar-fixed-top',
-                //'style' => 'background-color:#B41F21',
-            ],
-        ]);
-        echo Nav::widget([
-            'options' => ['class' => 'navbar-nav navbar-right'],
-            'items' => [
-                ['label'=> 'Home', 'url'=>Yii::$app->homeUrl],
-                ['label'=> 'Projects', 'url'=>'/site/projects'],
-                // ['label'=> 'Home', 'url'=>Yii::$app->homeUrl, 'options' => ['style' => 'background-color: #B41F21;']],
-                // ['label'=> 'Projects', 'url'=>'/site/projects','options' => ['style' => 'color: #B41F21;']]
-            ],
-        ]);
-        NavBar::end();
-        ?>
-    </section>
+	<?php
+	NavBar::begin([
+		'brandLabel' => 'bare',
+		'brandUrl' => Yii::$app->homeUrl,
+		'options' => [
+			'class' => 'navbar-inverse navbar-fixed-top',
+			//'style' => 'background-color:#B41F21',
+		],
+	]);
+	echo Nav::widget([
+		'options' => ['class' => 'navbar-nav navbar-right'],
+		'items' => [
+			['label'=> 'Home', 'url'=>Yii::$app->homeUrl],
+		],
+	]);
+	NavBar::end();
+	?>
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= $content ?>
-    </div>
-
-    <div class="not-set">
-        <p>Hallo</p>
     </div>
 </div>
 
@@ -66,7 +56,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Klushok  <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Hoco  <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
