f45e26e14d88e8891a785cf3a1313f6e7fedad5a
Bram <bramdenouden96@hotmail.com>
2019-09-08 20:09:24 +0200

    Initial commit based on barebone-yii template

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -25,35 +25,48 @@ AppAsset::register($this);
 
 <body>
 <?php $this->beginBody() ?>
-
 <div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Bare',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-        ],
-    ]);
-    NavBar::end();
-    ?>
-
+    <section id='klushok-navbar'>
+        <?php
+        NavBar::begin([
+            'brandLabel' => Html::img('@web/img/klushok-logo.png',['alt'=>Yii::$app->name, 'class' => "logo_flag"]),
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'id' => 'klushok-navbar'
+                //'class' => 'navbar-inverse navbar-fixed-top',
+                //'style' => 'background-color:#B41F21',
+            ],
+        ]);
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav navbar-right'],
+            'items' => [
+                ['label'=> 'Home', 'url'=>Yii::$app->homeUrl],
+                ['label'=> 'Projects', 'url'=>'/site/projects'],
+                // ['label'=> 'Home', 'url'=>Yii::$app->homeUrl, 'options' => ['style' => 'background-color: #B41F21;']],
+                // ['label'=> 'Projects', 'url'=>'/site/projects','options' => ['style' => 'color: #B41F21;']]
+            ],
+        ]);
+        NavBar::end();
+        ?>
+    </section>
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= $content ?>
     </div>
+
+    <div class="not-set">
+        <p>Hallo</p>
+    </div>
 </div>
+
+
+
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; HoCo  <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Klushok  <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
